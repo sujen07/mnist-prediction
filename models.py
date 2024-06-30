2b2e70e9e3cf55bee5848909380c47e0b0cc4be9
sujen07 <sujenk@gmail.com>
2024-06-30 11:20:45 -0700

    change model to supported layers in onnx

--- models.py
+++ models.py
@@ -6,22 +6,16 @@ import torch.nn.functional as F
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, 5)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        # Adaptive pooling to a fixed output size
-        self.adaptive_pool = nn.AdaptiveAvgPool2d((4, 4))
-        # Fully connected layers
-        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.conv1 = nn.Conv2d(1, 6, 5, stride=2, padding=2)  
+        self.conv2 = nn.Conv2d(6, 16, 5, stride=2, padding=2)  
+        self.fc1 = nn.Linear(16 * 7 * 7, 120)  
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2)
         x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2)
-        x = self.adaptive_pool(x) 
-        x = x.view(x.size(0), -1) 
+        x = x.view(x.size(0), -1)  # Flatten the tensor
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
